da4b684ee52ade23394216afa589ac745f3f7ce7
Martin Carpella <martin.carpella@gmx.net>
2024-11-22 22:46:06 +0000

    elm327: polling HV SoC once on startup

--- wican-elm327-evcc-mqtt-dacia.py
+++ wican-elm327-evcc-mqtt-dacia.py
@@ -7,11 +7,17 @@ from typing import Optional
 WICAN_IP = "192.168.1.92"
 WICAN_ELM327_PORT = 3333
 
-FORMAT = '%(asctime)s %(name)-15s %(message)s'
+FORMAT = '%(asctime)s %(name)-15s %(levelname)-7s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 
-COMM_LOG = logging.getLogger("elm327")
+COMM_LOG = logging.getLogger("elm327.comm")
 COMM_LOG.setLevel(logging.WARNING)
+
+SESSION_LOG = logging.getLogger("elm327.session")
+SESSION_LOG.setLevel(logging.INFO)
+
+CON_LOG = logging.getLogger("elm327.con")
+CON_LOG.setLevel(logging.INFO)
 
 
 class Elm327Communicator:
@@ -19,13 +25,21 @@ class Elm327Communicator:
         assert socket
         self._socket = socket
 
-    def send_cmd_get_first_line(self, cmd: bytes):
+    def send_cmd_get_first_line(self, cmd: bytes) -> bytes:
         response = self.send_cmd_and_read_until(cmd, b'>')
         idx = response.find(b'\r')
-        first_line = ""
+        first_line = b""
         if idx >= 0:
             first_line = response[0:idx]
         return first_line
+
+    def send_cmd_get_lines(self, cmd: bytes) -> list[bytes]:
+        response = self.send_cmd_and_read_until(cmd, b'>')
+        res = []
+        for line in response.split(b'\r'):
+            if len(line) > 0 and line[0] != b'>'[0]:
+                res.append(line)
+        return res
 
     def send_cmd_and_expect(self, cmd: bytes, expected=b"OK"):
         first_line = self.send_cmd_get_first_line(cmd)
@@ -74,21 +88,21 @@ class Elm327Session:
         pass
 
     def initialize_or_reset(self):
-        logging.info("Resetting and reinitializing ELM327...")
+        SESSION_LOG.info("Resetting and reinitializing ELM327...")
         try:
             self._comm.send_cmd_and_read_until(b"ATZ", b">")
             # send twice, if previous session was stuck in a strange state,
             # first send might not recognize ATZ as start of command
             self._comm.send_cmd_and_read_until(b"ATZ", b">")
         except TimeoutError:
-            logging.info("Reset timed out. Trying one more time.")
+            SESSION_LOG.info("Reset timed out. Trying one more time.")
             self._comm.send_cmd_and_read_until(b"ATZ", b">")
         self._comm.send_cmd_and_read_until(b"ATL0", b">")
         for cmd in Elm327Session.INIT_COMMANDS:
             ok, first_line = self._comm.send_cmd_and_expect(cmd, b"OK")
             if not ok:
-                logging.warning("INIT ERROR: %s not acknowledged: %s", cmd, first_line)
-        logging.info("Initialization of adapter done.")
+                SESSION_LOG.warning("INIT ERROR: %s not acknowledged: %s", cmd, first_line)
+        SESSION_LOG.info("Initialization of adapter done.")
 
     def read_device_battery_voltage(self) -> float:
         v = self._comm.send_cmd_get_first_line(b"ATRV")
@@ -97,6 +111,18 @@ class Elm327Session:
         if v[-1] == ord("V"):
             v = v[0:-1]
         return float(v)
+
+    def read_hv_battery_soc(self) -> float:
+        res = self._comm.send_cmd_get_first_line(b"015B")
+        if res == b'NO DATA':
+            SESSION_LOG.warning("Querying HV battery SoC: NO DATA")
+            return 0.0
+        if len(res) != 16:
+            SESSION_LOG.warning("015B: Unsupported response length %s for %s", len(res), res)
+            return 0.0
+        value_byte_str = res[-2:]  # last 2 hex chars
+        value_byte = int(value_byte_str, 16)
+        return float(value_byte) * 100 / 255
 
 
 class Elm327Connection:
@@ -120,18 +146,18 @@ class Elm327Connection:
     def connect(self) -> bool:
         self.close()
         try:
-            logging.debug(f"Connecting to {self.host}:{self.port}...")
+            CON_LOG.debug(f"Connecting to {self.host}:{self.port}...")
             self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self._socket.settimeout(self.timeout)
             self._socket.connect((self.host, self.port))
             self._connected = True
-            logging.info(f"Connected to {self.host}:{self.port}")
+            CON_LOG.info(f"Connected to {self.host}:{self.port}")
         except socket.timeout:
-            logging.debug(f"Timeout occurred. Unable to connect within {self.timeout} seconds.")
+            CON_LOG.debug(f"Timeout occurred. Unable to connect within {self.timeout} seconds.")
         except ConnectionRefusedError:
-            logging.warning("The server is not accepting connections from this host or port.")
+            CON_LOG.warning("The server is not accepting connections from this host or port.")
         except Exception as e:
-            logging.warning(f"An error occurred: {str(e)}")
+            CON_LOG.warning(f"An error occurred: {str(e)}")
         return self._connected
 
     def close(self) -> None:
@@ -141,9 +167,9 @@ class Elm327Connection:
                 self._socket.close()
                 self._socket = None
                 if self._connected:
-                    logging.info(f"Disconnected from {self.host}:{self.port}")
+                    CON_LOG.info(f"Disconnected from {self.host}:{self.port}")
         except Exception as e:
-            logging.warning(f"Failed closing previous socket: {e}")
+            CON_LOG.warning(f"Failed closing previous socket: {e}")
         finally:
             self._connected = False
 
@@ -153,23 +179,29 @@ class Elm327Connection:
         return Elm327Session(self._socket)
 
 
-def main_loop(con: Elm327Connection):
+def poll_loop(con: Elm327Connection):
     last_device_voltage = 0.0
+    last_soc_percentage = 0.0
     with con.new_session() as session:
+        last_soc_percentage = 0.0
         while True:
             logging.debug("Session loop start.")
             v = session.read_device_battery_voltage()
             if (last_device_voltage != v):
-                logging.info("Device voltage changed: %sV", v)
+                logging.info("Device voltage changed: %.1fV", v)
                 last_device_voltage = v
+
+            if last_soc_percentage == 0.0:
+                last_soc_percentage = session.read_hv_battery_soc()
+                if last_soc_percentage > 0:
+                    logging.info("HV Battery SoC: %.2f%%", last_soc_percentage)
+
             logging.debug("Session loop end. Sleep 3s.")
             time.sleep(3)
 
 
-cont = True
-while cont:
-    cont = False
-    logging.info("Waiting for car to be reachable...")
+while True:
+    logging.info("Waiting for elm327 device to be reachable...")
     with Elm327Connection(WICAN_IP, WICAN_ELM327_PORT) as con:
         while not con.connect():
             logging.debug("Not connected...")
@@ -177,7 +209,7 @@ while cont:
             time.sleep(1)
         logging.info("Connection to car established.")
         try:
-            main_loop(con)
+            poll_loop(con)
         except Exception as e:
             logging.warning("Error in main processing loop: %s", str(e))
     logging.info("Monitoring session completed.")
