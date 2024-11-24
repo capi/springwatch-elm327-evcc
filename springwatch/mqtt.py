107cebcad1a54a9206d8d913757da271d0f0c6e5
Martin Carpella <martin.carpella@gmx.net>
2024-11-24 21:09:46 +0000

    Only publish updated readings to MQTT

--- springwatch/mqtt.py
+++ springwatch/mqtt.py
@@ -1,3 +1,4 @@
+from datetime import datetime, UTC
 import logging
 import paho.mqtt.publish as publish
 import json
@@ -12,24 +13,30 @@ class MqttModelPublisher(ModelPublisher):
         self.host = host
         self.port = port
         self.base_topic = base_topic
+        self.publish_highwater_mark = datetime.fromtimestamp(0, tz=UTC)
 
     def publish(self, world: WorldView) -> None:
         MQTT_LOGGER.debug("Publishing to MQTT (host=%s, port=%s, base_topic=%s)", self.host, self.port, self.base_topic)
         try:
             readings = [world.battery_12v_voltage, world.battery_hv_soc_percent]
             msgs = []
+            hwm = self.publish_highwater_mark
             for reading in readings:
                 if reading.value is not None and world.is_from_current_session(reading):
                     assert reading.last_read
-                    topic = f"{self.base_topic}/{reading.short_name}"
-                    payload = {
-                        "value": reading.value,
-                        "when": reading.last_read.isoformat()
-                    }
-                    msg = {'topic': topic, 'payload': json.dumps(payload), 'retain': True}
-                    msgs.append(msg)
+                    if reading.last_read > self.publish_highwater_mark:
+                        topic = f"{self.base_topic}/{reading.short_name}"
+                        payload = {
+                            "value": reading.value,
+                            "when": reading.last_read.isoformat()
+                        }
+                        msg = {'topic': topic, 'payload': json.dumps(payload), 'retain': True}
+                        msgs.append(msg)
+                        if reading.last_read > hwm:
+                            hwm = reading.last_read
             if len(msgs) > 0:
                 publish.multiple(msgs, hostname=self.host, port=self.port)
                 MQTT_LOGGER.debug("Published %s messages.", len(msgs))
+                self.publish_highwater_mark = hwm
         except Exception as e:
             MQTT_LOGGER.warning("Failed publishing MQTT messages: %s", str(e))
